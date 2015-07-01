5799a9be502bd237ea0d338549bbbc33a0a2f946
Al3XKOoL <moonrotation@gmail.com>
2015-07-01 12:21:13 +0100

    Update Boardconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,15 +9,22 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a7
 ARCH_ARM_HAVE_VFP := true
 ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := cortex-a7
-DEVICE_RESOLUTION := 720x1280
 BOARD_HAS_NO_SELECT_BUTTON := true
 
-TARGET_BOOTLOADER_BOARD_NAME := mt6582
+TARGET_BOOTLOADER_BOARD_NAME := rainbow
 
+# MTK Hardware
+BOARD_HAS_MTK_HARDWARE := true
+MTK_HARDWARE := true
+COMMON_GLOBAL_CFLAGS += -DMTK_HARDWARE
+COMMON_GLOBAL_CPPFLAGS += -DMTK_HARDWARE
+
+BOARD_GPS_LIBRARIES := true
 
 # Partitions
 # make_ext4fs requires numbers in dec format
@@ -34,8 +41,6 @@ BOARD_KERNEL_CMDLINE :=
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 
-BOARD_CUSTOM_BOOTIMG_MK := device/wiko/rainbow/bootimg.mk
-
 # Flags
 TARGET_GLOBAL_CFLAGS   += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
@@ -46,11 +51,38 @@ COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
 TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
 
 TARGET_PREBUILT_KERNEL := device/wiko/rainbow/kernel
-TARGET_RECOVERY_FSTAB := device/wiko/rainbow/rootdir/root/recovery.fstab
+BOARD_CUSTOM_BOOTIMG_MK := device/wiko/rainbow/bootimg.mk
+BOARD_MKBOOTIMG_ARGS := --board 1336460062
+#TARGET_PREBUILT_RECOVERY_KERNEL :=
+
+TARGET_RECOVERY_FSTAB := device/wiko/rainbow/rootdir/root/twrp.fstab
+
+# TWRP
+DEVICE_RESOLUTION := 720x1280
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun%d/file
+TW_MAX_BRIGHTNESS := 255
+TW_INTERNAL_STORAGE_PATH := "/emmc"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "emmc"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p7"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data=ordered"
+TW_EXCLUDE_SUPERSU := true
+TW_INCLUDE_FB2PNG := true
 
 # Deodex
 WITH_DEXPREOPT := false
 DISABLE_DEXPREOPT := true
+
+#Camera
+USE_CAMERA_STUB := true
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
@@ -62,6 +94,8 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/wiko/rainbow/bluetooth
 BOARD_EGL_CFG := device/wiko/rainbow/rootdir/configs/egl.cfg
 USE_OPENGL_RENDERER := true
 BOARD_EGL_WORKAROUND_BUG_10194508 := true
+
+TARGET_CPU_MEMCPY_OPT_DISABLE := true
 
 # SELINUX
 BOARD_SEPOLICY_DIRS := \
